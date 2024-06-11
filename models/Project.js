06fa2f54093ea1b5c960c66dfe989c7e3acea598
walker <spadonival7@gmail.com>
2024-06-11 19:53:38 -0400

    added my project.js modal

---
+++ models/Project.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { type } = require('os');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true,
+    
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Description: {
+            type: DataTypes.STRING,
+        }, 
+        comment: {
+            type: DataTypes.STRING,
+          },
+          userId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            }
+          }
+        },
+        {
+          sequelize,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'project',
+        }
+      );
+      
+      module.exports = Project;
