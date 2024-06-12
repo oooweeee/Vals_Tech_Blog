475bb539a7acc0b3f197b75e005ac8bf7fd35a2d
walker <spadonival7@gmail.com>
2024-06-11 20:20:07 -0400

    added finished logout.js in public folder and psudo coded for login.js in public folder

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+// collect values from the login form
+
+// send a post request to the API endpoint
+
+// if it is successful, reirect the browser to the profile page
+
+}
